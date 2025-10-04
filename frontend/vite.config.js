f1af55395b53f7eab76e5e6d8b1cc333451d171d
Eviloux <44774088+Eviloux@users.noreply.github.com>
2025-10-04 21:00:19 +0200

    Rename Twitch redirect state ref

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   server: {
     port: 5173,
     headers: {
-      'Cross-Origin-Opener-Policy': 'unsafe-none',
       'Cross-Origin-Embedder-Policy': 'unsafe-none'
     }
   }
