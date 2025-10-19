f12d129f8fb4ebba1c2786b4e7a91ad2e5d9ce1c
Eviloux <44774088+Eviloux@users.noreply.github.com>
2025-10-19 19:23:10 +0200

    Tighten SPA fallback and clean admin nav

--- frontend/server.js
+++ frontend/server.js
@@ -84,17 +84,29 @@ const server = createServer(async (req, res) => {
   const requestUrl = new URL(req.url, `http://${req.headers.host ?? 'localhost'}`);
   let pathname = decodeURIComponent(requestUrl.pathname);
 
-  if (pathname === '/') {
-    res.statusCode = 302;
-    res.setHeader('Location', '/submit');
-    setCommonHeaders(res);
-    res.end();
-    return;
-  }
+  const methodAllowsSpaFallback = req.method === 'GET' || req.method === 'HEAD';
 
-  if (pathname === '/submit') {
-    sendFile(req, res, indexPath);
-    return;
+  if (methodAllowsSpaFallback) {
+    if (pathname === '/' || pathname === '') {
+      sendFile(req, res, indexPath);
+      return;
+    }
+
+    const looksLikeStaticAsset = extname(pathname) !== '';
+    if (!looksLikeStaticAsset) {
+      const acceptsHtml = (() => {
+        const acceptHeader = req.headers['accept'];
+        if (!acceptHeader) {
+          return true;
+        }
+        return acceptHeader.includes('text/html');
+      })();
+
+      if (acceptsHtml) {
+        sendFile(req, res, indexPath);
+        return;
+      }
+    }
   }
 
 
